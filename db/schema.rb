f60a21521b22a7d7a6fa6b0e795abeb4ec729853
kaiser5072 <kaiser5072@gmail.com>
2018-01-09 18:41:49 +0900

    Generate Bulletin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107124856) do
+ActiveRecord::Schema.define(version: 20180109093446) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
